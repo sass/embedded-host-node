cd3a892f93bb6845837fdb6458878ee1a19d1f3c
awjin <awjinahn@gmail.com>
2020-07-17 11:36:36 -0500

    Code review.

--- lib/src/embedded/compiler/process.ts
+++ lib/src/embedded/compiler/process.ts
@@ -19,28 +19,28 @@ export class EmbeddedProcess {
     }
   );
 
-  private readonly _stdout$ = new Subject<Buffer>();
-
-  private readonly _stderr$ = new Subject<Buffer>();
-
-  private readonly _exit$ = new Subject<number | null>();
+  // These Subjects are written to by the child process's stdio. They are
+  // publicly exposed as readonly Observables to prevent memory leaks.
+  private readonly rawStdout$ = new Subject<Buffer>();
+  private readonly rawStderr$ = new Subject<Buffer>();
+  private readonly rawExit$ = new Subject<number | null>();
 
   /** Sends the buffers it receives to the child process's stdin. */
   readonly stdin$ = new Subject<Buffer>();
 
   /** The buffers emitted by the child process's stdout. */
-  readonly stdout$ = this._stdout$.pipe(takeUntil(this._exit$));
+  readonly stdout$ = this.rawStdout$.pipe(takeUntil(this.rawExit$));
 
   /** The buffers emitted by the child process's stderr. */
-  readonly stderr$ = this._stderr$.pipe(takeUntil(this._exit$));
+  readonly stderr$ = this.rawStderr$.pipe(takeUntil(this.rawExit$));
 
   /** The child process's exit event. */
-  readonly exit$ = this._exit$.pipe(takeUntil(this._exit$));
+  readonly exit$ = this.rawExit$.pipe(takeUntil(this.rawExit$));
 
   constructor() {
     this.stdin$.subscribe(buffer => this.process.stdin.write(buffer));
-    this.process.stdout.on('data', buffer => this._stdout$.next(buffer));
-    this.process.stderr.on('data', buffer => this._stderr$.next(buffer));
+    this.process.stdout.on('data', buffer => this.rawStdout$.next(buffer));
+    this.process.stderr.on('data', buffer => this.rawStderr$.next(buffer));
     this.process.on('exit', () => this.close());
   }
 
@@ -48,8 +48,8 @@ export class EmbeddedProcess {
    * Kills the child process and cleans up all associated Observables.
    */
   close() {
-    this._exit$.next();
-    this._exit$.complete();
+    this.rawExit$.next();
+    this.rawExit$.complete();
     this.stdin$.complete();
     this.process.stdin.end();
   }

919cd013f11fbe2d06538cec31893b58be55f51d
awjin <awjinahn@gmail.com>
2020-09-10 14:19:37 -0500

    Code review.

--- lib/src/embedded/compiler/message-transformer.test.ts
+++ lib/src/embedded/compiler/message-transformer.test.ts
@@ -25,32 +25,12 @@ describe('encode', () => {
     input.setSource('a {b: c}');
     const request = new InboundMessage.CompileRequest();
     request.setString(input);
-    request.setId(1);
     const message = new InboundMessage();
     message.setCompilerequest(request);
 
     transformer.write$.next(message);
     encoded$.complete();
-    expect(await encoded).toEqual([
-      Buffer.from([
-        18,
-        14,
-        8,
-        1,
-        18,
-        10,
-        10,
-        8,
-        97,
-        32,
-        123,
-        98,
-        58,
-        32,
-        99,
-        125,
-      ]),
-    ]);
+    expect(await encoded).toEqual([Buffer.from(message.serializeBinary())]);
   });
 });
 
@@ -72,26 +52,14 @@ describe('decode', () => {
   afterEach(() => toDecode$.complete());
 
   it('decodes buffer to OutboundMessage', async () => {
-    toDecode$.next(
-      Buffer.from([
-        18,
-        14,
-        8,
-        1,
-        18,
-        10,
-        10,
-        8,
-        97,
-        32,
-        123,
-        98,
-        58,
-        32,
-        99,
-        125,
-      ])
-    );
+    const input = new InboundMessage.CompileRequest.StringInput();
+    input.setSource('a {b: c}');
+    const request = new InboundMessage.CompileRequest();
+    request.setString(input);
+    const message = new InboundMessage();
+    message.setCompilerequest(request);
+
+    toDecode$.next(Buffer.from(message.serializeBinary()));
     transformer.close();
     expect(
       (await decoded).getCompileresponse()?.getSuccess()?.getCss()
@@ -99,177 +67,50 @@ describe('decode', () => {
   });
 
   describe('protocol error', () => {
-    let protocolError: Promise<string>;
+    let error: Promise<string>;
 
     beforeEach(async () => {
-      protocolError = transformer.protocolError$
+      error = transformer.error$
         .pipe(toArray())
         .pipe(take(1))
         .toPromise()
-        .then(protocolErrors => protocolErrors[0].getMessage());
+        .then(errors => errors[0].message);
     });
 
     it('fails on invalid buffer', async () => {
       toDecode$.next(Buffer.from([-1]));
       transformer.close();
-      expect(await protocolError).toEqual('Invalid buffer');
+      expect(await error).toEqual('Compiler caused error: Invalid buffer.');
     });
 
     it('fails on empty message', async () => {
       toDecode$.next(Buffer.from(new OutboundMessage().serializeBinary()));
       transformer.close();
-      expect(await protocolError).toEqual(
-        'OutboundMessage.message is not set.'
+      expect(await error).toEqual(
+        'Compiler caused error: OutboundMessage.message is not set.'
       );
     });
 
-    describe('compile response', () => {
-      it('fails on missing id', async () => {
-        const response = new OutboundMessage.CompileResponse();
-        const message = new OutboundMessage();
-        message.setCompileresponse(response);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.CompileResponse.id is not set'
-        );
-      });
-
-      it('fails on missing result', async () => {
-        const response = new OutboundMessage.CompileResponse();
-        response.setId(1);
-        const message = new OutboundMessage();
-        message.setCompileresponse(response);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.CompileResponse.result is not set'
-        );
-      });
+    it('fails on compile response with missing result', async () => {
+      const response = new OutboundMessage.CompileResponse();
+      const message = new OutboundMessage();
+      message.setCompileresponse(response);
+      toDecode$.next(Buffer.from(message.serializeBinary()));
+      transformer.close();
+      expect(await error).toEqual(
+        'Compiler caused error: OutboundMessage.CompileResponse.result is not set.'
+      );
     });
 
-    describe('log event', () => {
-      it('fails on missing compilation id', async () => {
-        const event = new OutboundMessage.LogEvent();
-        const message = new OutboundMessage();
-        message.setLogevent(event);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.LogEvent.compilationId is not set'
-        );
-      });
-    });
-
-    describe('canonicalize request', () => {
-      it('fails on missing compilation id', async () => {
-        const request = new OutboundMessage.CanonicalizeRequest();
-        const message = new OutboundMessage();
-        message.setCanonicalizerequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.CanonicalizeRequest.compilationId is not set'
-        );
-      });
-
-      it('fails on missing importer id', async () => {
-        const request = new OutboundMessage.CanonicalizeRequest();
-        request.setCompilationId(1);
-        const message = new OutboundMessage();
-        message.setCanonicalizerequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.CanonicalizeRequest.importerId is not set'
-        );
-      });
-    });
-
-    describe('import request', () => {
-      it('fails on missing compilation id', async () => {
-        const request = new OutboundMessage.ImportRequest();
-        const message = new OutboundMessage();
-        message.setImportrequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.ImportRequest.compilationId is not set'
-        );
-      });
-
-      it('fails on missing importer id', async () => {
-        const request = new OutboundMessage.ImportRequest();
-        request.setCompilationId(1);
-        const message = new OutboundMessage();
-        message.setImportrequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.ImportRequest.importerId is not set'
-        );
-      });
-    });
-
-    describe('file import request', () => {
-      it('fails on missing compilation id', async () => {
-        const request = new OutboundMessage.FileImportRequest();
-        const message = new OutboundMessage();
-        message.setFileimportrequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.FileImportRequest.compilationId is not set'
-        );
-      });
-
-      it('fails on missing importer id', async () => {
-        const request = new OutboundMessage.FileImportRequest();
-        request.setCompilationId(1);
-        const message = new OutboundMessage();
-        message.setFileimportrequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.FileImportRequest.importerId is not set'
-        );
-      });
-    });
-
-    describe('function call request', () => {
-      it('fails on missing compilation id', async () => {
-        const request = new OutboundMessage.FunctionCallRequest();
-        const message = new OutboundMessage();
-        message.setFunctioncallrequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.FunctionCallRequest.compilationId is not set'
-        );
-      });
-
-      it('fails on missing identifier', async () => {
-        const request = new OutboundMessage.FunctionCallRequest();
-        request.setCompilationId(1);
-        const message = new OutboundMessage();
-        message.setFunctioncallrequest(request);
-
-        toDecode$.next(Buffer.from(message.serializeBinary()));
-        transformer.close();
-        expect(await protocolError).toEqual(
-          'OutboundMessage.FunctionCallRequest.identifier is not set'
-        );
-      });
+    it('fails on function call request with missing identifier', async () => {
+      const request = new OutboundMessage.FunctionCallRequest();
+      const message = new OutboundMessage();
+      message.setFunctioncallrequest(request);
+      toDecode$.next(Buffer.from(message.serializeBinary()));
+      transformer.close();
+      expect(await error).toEqual(
+        'Compiler caused error: OutboundMessage.FunctionCallRequest.identifier is not set.'
+      );
     });
   });
 });
--- lib/src/embedded/compiler/message-transformer.ts
+++ lib/src/embedded/compiler/message-transformer.ts
@@ -3,38 +3,27 @@
 // https://opensource.org/licenses/MIT.
 
 import {from, Subject, Observable} from 'rxjs';
-import {
-  InboundMessage,
-  OutboundMessage,
-  ProtocolError,
-} from '../../vendor/embedded_sass_pb';
-import {protocolError} from './utils';
+import {InboundMessage, OutboundMessage} from '../../vendor/embedded_sass_pb';
 
 /**
- * Encodes InboundMessages into protocol buffers and decodes OutboundMessages
- * from protocol buffers. Any Embedded Protocol violations that can be detected
- * at the message level are encapsulated here and reported as ProtocolErrors.
+ * Encodes InboundMessages into protocol buffers and decodes protocol buffers
+ * into OutboundMessages. Any Embedded Protocol violations that can be detected
+ * at the message level are encapsulated here and reported as errors.
  */
 export class MessageTransformer {
-  // The decoded messages and protocol errors are written to these Subjects.
-  // They are publicly exposed as readonly Observables to prevent memory leaks.
+  // The decoded messages and errors are written to these Subjects. They are
+  // publicly exposed as readonly Observables to prevent memory leaks.
   private readonly readInternal$ = new Subject<OutboundMessage>();
-  private readonly protocolErrorInternal$ = new Subject<ProtocolError>();
+  private readonly errorInternal$ = new Subject<Error>();
 
-  /**
-   * The OutboundMessages (decoded from protocol buffers). These are guaranteed
-   * to be valid, since invalid messages are reported as ProtocolErrors.
-   */
+  /** The OutboundMessages (decoded from protocol buffers). */
   readonly read$ = from(this.readInternal$);
 
-  /**
-   * Receives InboundMessages and encodes them into protocol buffers. Errors
-   * encountered during encoding are reported as ProtocolErrors.
-   */
+  /** Receives InboundMessages and encodes them into protocol buffers. */
   readonly write$ = new Subject<InboundMessage>();
 
-  /** The ProtocolErrors encountered while encoding/decoding messages. */
-  readonly protocolError$ = from(this.protocolErrorInternal$);
+  /** Any errors encountered while encoding/decoding messages. */
+  readonly error$ = from(this.errorInternal$);
 
   constructor(
     private readonly rawRead$: Observable<Buffer>,
@@ -46,177 +35,87 @@ export class MessageTransformer {
 
   // Decodes a buffer into an OutboundMessage.
   private decode(buffer: Buffer) {
-    let message;
     try {
-      message = toMessage(buffer);
+      const message = toMessage(buffer);
+      this.readInternal$.next(message);
     } catch (error) {
-      this.protocolErrorInternal$.next(error);
+      this.errorInternal$.next(error);
     }
-    this.readInternal$.next(message);
   }
 
-  // Encodes an InboundMessage into a protocol buffer.
+  // Encodes an InboundMessage into a buffer.
   private encode(message: InboundMessage) {
-    let buffer;
     try {
-      buffer = toBuffer(message);
+      const buffer = Buffer.from(message.serializeBinary());
+      this.rawWrite$.next(buffer);
     } catch (error) {
-      this.protocolErrorInternal$.next(error);
+      this.errorInternal$.next(error);
     }
-    this.rawWrite$.next(buffer);
   }
 
   /** Cleans up all Observables. */
   close() {
     this.readInternal$.complete();
     this.write$.complete();
-    this.protocolErrorInternal$.complete();
+    this.errorInternal$.complete();
   }
 }
 
-// Encodes an InboundMessage into a protocol buffer. Throws a ProtocolError
-// if any errors are encountered.
-function toBuffer(message: InboundMessage): Buffer {
-  try {
-    return Buffer.from(message.serializeBinary());
-  } catch (error) {
-    throw protocolError(
-      ProtocolError.ErrorType.INTERNAL,
-      'Could not serialize buffer'
-    );
-  }
-}
-
-// Decodes an OutboundMessage from a protocol buffer. Throws a ProtocolError
-// if any errors are encountered.
+// Decodes a protocol buffer into an OutboundMessage, ensuring that all
+// mandatory message fields are populated. Throws if buffer cannot be decoded
+// into a valid message.
 function toMessage(buffer: Buffer): OutboundMessage {
   let message;
-
   try {
     message = OutboundMessage.deserializeBinary(buffer);
-  } catch (error) {
-    throw protocolError(ProtocolError.ErrorType.PARSE, 'Invalid buffer');
+  } catch {
+    throw compilerError('Invalid buffer.');
   }
 
-  // Ensure that the InboundMessage contains a valid message, and that all
-  // mandatory fields are populated.
   switch (message.getMessageCase()) {
+    case OutboundMessage.MessageCase.ERROR:
+    case OutboundMessage.MessageCase.LOGEVENT:
+    case OutboundMessage.MessageCase.CANONICALIZEREQUEST:
+    case OutboundMessage.MessageCase.IMPORTREQUEST:
+    case OutboundMessage.MessageCase.FILEIMPORTREQUEST:
+      break;
+
     case OutboundMessage.MessageCase.COMPILERESPONSE: {
-      const response = message.getCompileresponse()!;
-      if (!response.getId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.CompileResponse.id is not set'
-        );
-      }
       if (
-        response.getResultCase() ===
+        message.getCompileresponse()!.getResultCase() ===
         OutboundMessage.CompileResponse.ResultCase.RESULT_NOT_SET
       ) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.CompileResponse.result is not set'
-        );
-      }
-      break;
-    }
-
-    case OutboundMessage.MessageCase.LOGEVENT: {
-      const event = message.getLogevent()!;
-      if (!event.getCompilationId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.LogEvent.compilationId is not set'
-        );
-      }
-      break;
-    }
-
-    case OutboundMessage.MessageCase.CANONICALIZEREQUEST: {
-      const request = message.getCanonicalizerequest()!;
-      if (!request.getCompilationId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.CanonicalizeRequest.compilationId is not set'
-        );
-      }
-      if (!request.getImporterId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.CanonicalizeRequest.importerId is not set'
-        );
-      }
-      break;
-    }
-
-    case OutboundMessage.MessageCase.IMPORTREQUEST: {
-      const request = message.getImportrequest()!;
-      if (!request.getCompilationId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.ImportRequest.compilationId is not set'
-        );
-      }
-      if (!request.getImporterId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.ImportRequest.importerId is not set'
-        );
-      }
-      break;
-    }
-
-    case OutboundMessage.MessageCase.FILEIMPORTREQUEST: {
-      const request = message.getFileimportrequest()!;
-      if (!request.getCompilationId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.FileImportRequest.compilationId is not set'
-        );
-      }
-      if (!request.getImporterId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.FileImportRequest.importerId is not set'
+        throw compilerError(
+          'OutboundMessage.CompileResponse.result is not set.'
         );
       }
       break;
     }
 
     case OutboundMessage.MessageCase.FUNCTIONCALLREQUEST: {
-      const request = message.getFunctioncallrequest()!;
-      if (!request.getCompilationId()) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.FunctionCallRequest.compilationId is not set'
-        );
-      }
       if (
-        request.getIdentifierCase() ===
+        message.getFunctioncallrequest()!.getIdentifierCase() ===
         OutboundMessage.FunctionCallRequest.IdentifierCase.IDENTIFIER_NOT_SET
       ) {
-        throw protocolError(
-          ProtocolError.ErrorType.PARAMS,
-          'OutboundMessage.FunctionCallRequest.identifier is not set'
+        throw compilerError(
+          'OutboundMessage.FunctionCallRequest.identifier is not set.'
         );
       }
       break;
     }
 
     case OutboundMessage.MessageCase.MESSAGE_NOT_SET: {
-      throw protocolError(
-        ProtocolError.ErrorType.PARSE,
-        'OutboundMessage.message is not set.'
-      );
+      throw compilerError('OutboundMessage.message is not set.');
     }
 
     default: {
-      throw protocolError(
-        ProtocolError.ErrorType.PARSE,
-        `Unknown message type: ${message.toString()}`
-      );
+      throw compilerError(`Unknown message type ${message.toString()}.`);
     }
   }
 
   return message;
+}
+
+function compilerError(message: string) {
+  return new Error(`Compiler caused error: ${message}`);
 }

e2830ccbe7aa5d0493f87556160fe2df4c0e8d0f
Natalie Weizenbaum <nweiz@google.com>
2020-12-23 17:16:15 -0800

    Switch to GitHub actions
    This allows us to use both Dart and Node.js without having to manually
    install either one.

--- lib/src/embedded/packet-transformer.ts
+++ lib/src/embedded/packet-transformer.ts
@@ -2,7 +2,6 @@
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import '../buffer-builder';
 import {Observable, Subject} from 'rxjs';
 import {mergeMap} from 'rxjs/operators';
 import BufferBuilder = require('buffer-builder');

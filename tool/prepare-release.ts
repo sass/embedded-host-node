b5f8e8167c3fce91a10d014168d8aa068edba922
awjin <awjinahn@gmail.com>
2021-04-15 14:02:26 -0700

    Prepare first beta release to npm.

--- tool/prepare-release.ts
+++ tool/prepare-release.ts
@@ -5,12 +5,15 @@
 import {promises as fs} from 'fs';
 import * as shell from 'shelljs';
 
+import * as pkg from '../package.json';
 import {getEmbeddedProtocol} from './utils';
 
 shell.config.fatal = true;
 
 (async () => {
   try {
+    await sanityCheckBeforeRelease();
+
     await getEmbeddedProtocol({outPath: 'lib/src/vendor', release: true});
 
     console.log('Transpiling TS into dist.');
@@ -27,3 +30,27 @@ shell.config.fatal = true;
     process.exitCode = 1;
   }
 })();
+
+// Quick sanity checks to make sure the release we are preparing is a suitable
+// candidate for release.
+async function sanityCheckBeforeRelease() {
+  console.log('Running sanity checks before releasing.');
+  const releaseVersion = pkg.version;
+
+  const ref = process.env['GITHUB_REF'];
+  if (ref !== `refs/tags/${releaseVersion}`) {
+    throw Error(
+      `GITHUB_REF ${ref} is different than the package.json version ${releaseVersion}.`
+    );
+  }
+
+  if (releaseVersion.indexOf('-dev') > 0) {
+    throw Error(`${releaseVersion} is a dev release.`);
+  }
+
+  const versionHeader = new RegExp(`^## ${releaseVersion}$`, 'm');
+  const changelog = await fs.readFile('../CHANGELOG.md', 'utf8');
+  if (!changelog.match(versionHeader)) {
+    throw Error(`There's no CHANGELOG entry for ${releaseVersion}.`);
+  }
+}

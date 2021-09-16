b071ff427cc1caea4008a85ec613af11aebeea4c
Natalie Weizenbaum <nweiz@google.com>
2021-09-16 22:47:10 +0000

    Add an info field (#64)
    See sass/sass#3155

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,8 @@
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import * as pkg from '../package.json';
+
 export {Value} from './src/value/value';
 export {SassBoolean, sassFalse, sassTrue} from './src/value/boolean';
 export {sassNull} from './src/value/null';
@@ -14,3 +16,5 @@ export {
   RenderResult,
   RenderError,
 } from './src/node-sass/render';
+
+export const info = `sass-embedded\t${pkg.version}`;

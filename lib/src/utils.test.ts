60b76a1bbde7156d235e1947c5815c6ea992504e
なつき <i@ntk.me>
2022-04-25 23:52:24 -0700

    Add test cases for already encoded string

--- lib/src/utils.test.ts
+++ lib/src/utils.test.ts
@@ -15,5 +15,23 @@ describe('utils', () => {
         );
       }
     });
+
+    it('encode percent encoded string like `pathToFileURL`', () => {
+      const baseURL = pathToFileURL('').toString();
+      for (let i = 0; i < 128; i++) {
+        const lowercase = `%${i < 10 ? '0' : ''}${i.toString(16)}`;
+        expect(pathToUrlString(lowercase)).toEqual(
+          pathToFileURL(lowercase)
+            .toString()
+            .slice(baseURL.length + 1)
+        );
+        const uppercase = lowercase.toUpperCase();
+        expect(pathToUrlString(uppercase)).toEqual(
+          pathToFileURL(uppercase)
+            .toString()
+            .slice(baseURL.length + 1)
+        );
+      }
+    });
   });
 });

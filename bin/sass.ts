0093f6858b6e8cea26148c795274331217c3ce96
Natalie Weizenbaum <nweiz@google.com>
2024-08-01 13:14:01 -0700

    Add a Sass executable wrapper that forwards to the correct exe (#313)
    This allows users to run `npx sass` when they've just installed
    `sass-embedded` and directly run the Dart VM. Due to npm/cmd-shim#152,
    there's no way to make this work on Windows CMD/Powershell without
    substantially curtailing the performance on other operating systems.

---
+++ bin/sass.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import * as child_process from 'child_process';
+import {compilerCommand} from '../lib/src/compiler-path';
+
+// TODO npm/cmd-shim#152 and yarnpkg/berry#6422 - If and when the package
+// managers support it, we should make this a proper shell script rather than a
+// JS wrapper.
+
+try {
+  child_process.execFileSync(
+    compilerCommand[0],
+    [...compilerCommand.slice(1), ...process.argv.slice(2)],
+    {
+      stdio: 'inherit',
+      windowsHide: true,
+    }
+  );
+} catch (error) {
+  if (error.code) {
+    throw error;
+  } else {
+    process.exitCode = error.status;
+  }
+}

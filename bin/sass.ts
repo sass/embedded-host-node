5ddd62fb8da3e010d26e2e460cab0d61f830496d
Natalie Weizenbaum <nweiz@google.com>
2024-08-01 13:00:45 -0700

    Convert bin/sass to TypeScript

--- bin/sass.ts
+++ bin/sass.ts
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 
-const child_process = require("child_process");
-const {compilerCommand} = require("../dist/lib/src/compiler-path");
+import * as child_process from 'child_process';
+import {compilerCommand} from '../lib/src/compiler-path';
 
 // TODO npm/cmd-shim#152 and yarnpkg/berry#6422 - If and when the package
 // managers support it, we should make this a proper shell script rather than a
 // JS wrapper.
 
 try {
-child_process.execFileSync(
-  compilerCommand[0], [...compilerCommand.slice(1), ...process.argv.slice(2)], {
-    stdio: 'inherit',
-    windowsHide: true
-});
+  child_process.execFileSync(
+    compilerCommand[0],
+    [...compilerCommand.slice(1), ...process.argv.slice(2)],
+    {
+      stdio: 'inherit',
+      windowsHide: true,
+    }
+  );
 } catch (error) {
   if (error.code) {
     throw error;
@@ -20,5 +23,3 @@ child_process.execFileSync(
     process.exitCode = error.status;
   }
 }
-
-

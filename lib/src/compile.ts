7868c68bb5b2fcce34501c97c0d2851a92a73432
なつき <i@ntk.me>
2026-01-28 20:31:27 -0800

    Emit deprecation

--- lib/src/compile.ts
+++ lib/src/compile.ts
@@ -5,7 +5,12 @@
 import {initAsyncCompiler} from './compiler/async';
 import {OptionsWithLegacy, StringOptionsWithLegacy} from './compiler/utils';
 import {initCompiler} from './compiler/sync';
-import {CompileResult} from './vendor/sass';
+import {CompileResult, SourceMapIncludeSources} from './vendor/sass';
+import {
+  DeprecationOptions,
+  deprecations,
+  warnForHostSideDeprecation,
+} from './deprecations';
 
 export {NodePackageImporter} from './importer-registry';
 
@@ -13,6 +18,11 @@ export function compile(
   path: string,
   options?: OptionsWithLegacy<'sync'>,
 ): CompileResult {
+  _warnForSourceMapIncludeSourcesBoolean(
+    options?.sourceMapIncludeSources,
+    options?.legacy,
+    options,
+  );
   const compiler = initCompiler();
   try {
     return compiler.compile(path, options);
@@ -25,6 +35,11 @@ export function compileString(
   source: string,
   options?: StringOptionsWithLegacy<'sync'>,
 ): CompileResult {
+  _warnForSourceMapIncludeSourcesBoolean(
+    options?.sourceMapIncludeSources,
+    options?.legacy,
+    options,
+  );
   const compiler = initCompiler();
   try {
     return compiler.compileString(source, options);
@@ -37,6 +52,11 @@ export async function compileAsync(
   path: string,
   options?: OptionsWithLegacy<'async'>,
 ): Promise<CompileResult> {
+  _warnForSourceMapIncludeSourcesBoolean(
+    options?.sourceMapIncludeSources,
+    options?.legacy,
+    options,
+  );
   const compiler = await initAsyncCompiler();
   try {
     return await compiler.compileAsync(path, options);
@@ -49,10 +69,36 @@ export async function compileStringAsync(
   source: string,
   options?: StringOptionsWithLegacy<'async'>,
 ): Promise<CompileResult> {
+  _warnForSourceMapIncludeSourcesBoolean(
+    options?.sourceMapIncludeSources,
+    options?.legacy,
+    options,
+  );
   const compiler = await initAsyncCompiler();
   try {
     return await compiler.compileStringAsync(source, options);
   } finally {
     await compiler.dispose();
+  }
+}
+
+function _warnForSourceMapIncludeSourcesBoolean(
+  sourceMapIncludeSources?: SourceMapIncludeSources | boolean,
+  legacy?: boolean,
+  options?: DeprecationOptions,
+): void {
+  if (
+    legacy !== true &&
+    (sourceMapIncludeSources === true || sourceMapIncludeSources === false)
+  ) {
+    const suggestion = sourceMapIncludeSources ? 'always' : 'never';
+    warnForHostSideDeprecation(
+      'Passing a boolean value for Options.sourceMapIncludeSources is' +
+        'deprecated and will be removed in Dart Sass 2.0.0.\n' +
+        `Please use '${suggestion}' instead of ${sourceMapIncludeSources}.\n\n` +
+        'More info: https://sass-lang.com/d/source-map-include-sources-boolean',
+      deprecations['source-map-include-sources-boolean'],
+      options,
+    );
   }
 }
